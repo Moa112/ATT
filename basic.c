d3a4fe28f3d04f419aecba2798cf660fe0a3d563
Cícero Moacir Soares Rodrigues <119634016+Moa112@users.noreply.github.com>
2024-04-13 13:00:13 -0300

    Primeiro

---
+++ basic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int x = 0;
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &x);
+
+    printf("A sua idade eh: %d\n", x);
+
+    return 0;
+}
